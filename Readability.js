f2d4d2a82db9d43aef6a423706dc668309ea47b4
tent4kel <48975759+tent4kel@users.noreply.github.com>
2025-01-29 08:33:43 +0100

    Revert Readability.js

--- Readability.js
+++ Readability.js
@@ -39,7 +39,7 @@ function Readability(doc, options) {
   this._doc = doc;
   this._docJSDOMParser = this._doc.firstChild.__JSDOMParser__;
   this._articleTitle = null;
-  this._articleByline = "";
+  this._articleByline = null;
   this._articleDir = null;
   this._articleSiteName = null;
   this._attempts = [];
@@ -142,11 +142,11 @@ Readability.prototype = {
     okMaybeItsACandidate: /and|article|body|column|content|main|shadow/i,
 
     positive:
-      /article|body|content|entry|hentry|h-entry|main|page|pagination|post|hero|text|blog|story/i,
+      /article|body|content|entry|hentry|h-entry|main|page|pagination|post|text|blog|story/i,
     negative:
-      /-ad-|hidden|^hid$| hid$| hid |^hid |banner|combx|comment|com-|contact|footer|gdpr|masthead|media|meta|outbrain|promo|related|scroll|share|shoutbox|sidebar|skyscraper|sponsor|shopping|feature-bar|tags|widget/i,
+      /-ad-|hidden|^hid$| hid$| hid |^hid |banner|combx|comment|com-|contact|footer|gdpr|masthead|media|meta|outbrain|promo|related|scroll|share|shoutbox|sidebar|skyscraper|sponsor|shopping|tags|widget/i,
     extraneous:
-      /print|archive|comment|discuss|e[\-]?mail|share|reply|all|sign|single|utility/i,
+      /print|archive|comment|discuss|e[\-]?mail|share|reply|all|login|sign|single|utility/i,
     byline: /byline|author|dateline|writtenby|p-author/i,
     replaceFonts: /<(\/?)font[^>]*>/gi,
     normalize: /\s{2,}/g,
@@ -154,7 +154,7 @@ Readability.prototype = {
       /\/\/(www\.)?((dailymotion|youtube|youtube-nocookie|player\.vimeo|v\.qq)\.com|(archive|upload\.wikimedia)\.org|player\.twitch\.tv)/i,
     shareElements: /(\b|_)(share|sharedaddy)(\b|_)/i,
     nextLink: /(next|weiter|continue|>([^\|]|$)|»([^\|]|$))/i,
-    prevLink: /(prev|zurück|old|new|<|«)/i,
+    prevLink: /(prev|earl|old|new|<|«)/i,
     tokenize: /\W+/g,
     whitespace: /^\s*$/,
     hasContent: /\S$/,
@@ -179,7 +179,6 @@ Readability.prototype = {
     "menubar",
     "complementary",
     "navigation",
-    "feature-bar",
     "alert",
     "alertdialog",
     "dialog",
