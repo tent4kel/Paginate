3ce1c012b63cdc354bd2b2b6f8a7de4aeee014bf
tent4kel <48975759+tent4kel@users.noreply.github.com>
2025-02-02 22:54:36 +0100

    Update columnate.js

--- columnate.js
+++ columnate.js
@@ -40,7 +40,7 @@ function Columnate() {
     CleanHTML(doclone);
 
     try {
-        var article = new Readability(doclone, debug).parse();
+         var article = new Readability(doclone, { debug: true }).parse();
         
 
         var htmltag = document.getElementsByTagName("html")[0];
