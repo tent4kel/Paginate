6f5274c035cf6bc3ba358adb47681a051a2db565
tent4kel <48975759+tent4kel@users.noreply.github.com>
2025-02-08 22:46:26 +0100

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -61,10 +61,10 @@
 let swipeTimer = null;
 let swipeDirection = 0; // 1 for forward, -1 for backward
 let swipeStartX = 0, swipeStartY = 0;
-const baseInterval = 500; // base interval in ms
-const minInterval = 100;  // fastest possible interval
+const baseInterval = 700; // base interval in ms
+const minInterval = 200;  // fastest possible interval
 const threshold = 50;     // minimum swipe distance to start paging
-const scaleFactor = 0.2;  // each additional pixel reduces interval by 0.2ms
+const scaleFactor = 0.1;  // each additional pixel reduces interval by 0.2ms
 let dynamicInterval = baseInterval;
 
 document.addEventListener('touchstart', function(event) {
